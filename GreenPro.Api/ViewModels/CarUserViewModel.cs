3f70505ffc0a30449574445a377c763ec6450b0e
circussite <circussite1@gmail.com>
2016-10-16 20:18:51 -0700

    Car Users View Model/ Response

---
+++ GreenPro.Api/ViewModels/CarUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenPro.Api.ViewModels
+{
+    public class CarUserViewModel
+    {
+        public int CarId { get; set; }
+        public string DisplayName { get; set; }
+        public string Make { get; set; }
+        public string LicenseNumber { get; set; }
+        public string Color { get; set; }
+        public int PurchaseYear { get; set; }
+        public string UserId { get; set; }
+        public bool AutoRenewal { get; set; }
+        public bool IsDeleted { get; set; }
+        public Nullable<int> GarageId { get; set; }
+    }
+}
---
+++ GreenPro.Api/ViewModels/UserCarListResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenPro.Api.ViewModels
+{
+    public class UserCarListResponse
+    {
+        public UserCarListResponse()
+        {
+            cars = new List<CarUserViewModel>();
+        }
+
+        public IList<CarUserViewModel> cars { get; set; }
+    }
+}
